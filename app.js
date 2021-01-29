1b97cddd74d41353fded9a0080de5d18255fddeb
zhendozzz <info@zhendozzz.ru>
2021-01-29 12:15:13 +0300

    feature add telegram bot

--- app.js
+++ app.js
@@ -3,36 +3,40 @@ const app = express();
 const routes = require("./routes/index");
 const path = require('path');
 const jwt = require("jsonwebtoken");
-
+const cors = require('cors');
 const tokenKey = "1a2b-3c4d-5e6f-7g8h";
 
+const Telegram = require('./telegram.js');
+const telegram = new Telegram();
+
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use((req, res, next) => {
-  if (req.headers.authorization) {
-    jwt.verify(
-      req.headers.authorization.split(" ")[1],
-      tokenKey,
-      (err, payload) => {
-        if (err) next();
-        else if (payload) {
-          for (let user of users) {
-            if (user.id === payload.id) {
-              req.user = user;
-              next();
+    if (req.headers.authorization) {
+        jwt.verify(
+            req.headers.authorization.split(" ")[1],
+            tokenKey,
+            (err, payload) => {
+                if (err) next();
+                else if (payload) {
+                    for (let user of users) {
+                        if (user.id === payload.id) {
+                            req.user = user;
+                            next();
+                        }
+                    }
+
+                    if (!req.user) next();
+                }
             }
-          }
+        );
+    }
 
-          if (!req.user) next();
-        }
-      }
-    );
-  }
-
-  next();
+    next();
 });
 
 app.use("/api", routes);
--- telegram.js
+++ telegram.js
@@ -1,16 +1,105 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.autoprofibot_token;
-const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    const chatId = msg.chat.id;
-    const resp = match[1];
-    bot.sendMessage(chatId, resp);
-});
+class Telegram {
+    bot;
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    console.log(msg);
-    bot.sendMessage(chatId, 'sfd');
-});
-exports.module = bot;
+    commands = {
+        '/start': (msg) => {
+            this.bot.sendMessage(msg.chat.id, 'Вы подписались')
+        },
+        '/booking': (msg) => {
+            const chatId = msg.chat.id;
+            const opt = this.buildKeyboardMessage(
+                this.buildKeyboardVariants(
+                    [{
+                        text: '16:30',
+                        date: '21.01.2021'
+                    }]
+                )
+            );
+            this.bot.sendMessage(chatId, 'text', opt);
+        }
+    };
+
+    callBackQueries = {};
+
+    events = {
+        'message': (msg) => {
+            this.processMessage(msg);
+        },
+        'polling_error': (err) => {
+            console.log(err);
+        },
+        'callback_query': (msg) => {
+            this.processCallBackQuery(msg);
+        }
+    };
+
+    constructor() {
+        const autoProfiTelegramBotToken = process.env.autoprofi_telegram_bot_token;
+        this.bot = new TelegramBot(autoProfiTelegramBotToken, {
+            polling: true,
+            filepath: false,
+        });
+        this.registerEvents();
+    }
+
+    registerEvents() {
+        for (const event in this.events) {
+            this.bot.on(event, this.events[event]);
+        }
+    }
+
+    processMessage(msg) {
+        const text = msg.text;
+        const command = this.commands[text];
+        if (command !== undefined) {
+            command(msg);
+        }
+    }
+
+    processCallBackQuery(msg) {
+        const data = JSON.parse(msg.data);
+        //const {time, date} = data;
+        console.log(msg);
+    }
+
+    buildKeyboardVariants(objects = []) {
+        let result = [];
+        objects.forEach((object) => {
+            result.push({
+                text: object.text,
+                callback_data: JSON.stringify(object)
+            })
+
+        })
+        return result;
+    }
+
+    buildKeyboardMessage(variants = [], numberOnLines = 3) {
+        let counter = 0;
+        let keyboard = [];
+        let keyboardData = [];
+        for (let i = 0; i < variants.length; i++) {
+            counter++;
+            const variant = variants[i];
+            keyboardData.push(variant);
+            if (counter >= numberOnLines) {
+                keyboard.push(keyboardData);
+                keyboardData = [];
+                counter = 0;
+            }
+        }
+        keyboard.push(keyboardData);
+        const opt = {
+            parse_mode: 'markdown',
+            disable_web_page_preview: true,
+            reply_markup: JSON.stringify({
+                inline_keyboard: keyboard
+            })
+        };
+        return opt;
+    }
+}
+
+module.exports = Telegram;
